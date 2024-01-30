2c47c9651ca27aff8352ef0e627d35a44cb51478
DanLla0 <daniel.llamasparedes@riberadeltajo.es>
2024-01-30 11:06:06 +0100

    update

--- ClienteTCP.py
+++ ClienteTCP.py
@@ -1,5 +1,6 @@
 import socket
 import ipaddress
+import math
 
 HOST = "localhost"
 PORT = 2000
@@ -7,12 +8,19 @@ PORT = 2000
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM) # sock stream es que vamos a intercambiar datos
 s.connect((HOST,PORT))
 print(f"Conectado con exito")
-attempts = 10
+attempts = 4
+code = 0
 while attempts > 0:
-    n = input("Dime un número:")
-    s.send(n.encode())
-    print(s.recv(1024).decode())
-    if s.recv(1024).decode() == "1":
-        break
-    attempts = attempts-1
+        n = input("Dime un número:")
+        s.send(n.encode())
+        code = s.recv(1024).decode() 
+        if code == "-1":
+            print(s.recv(1024).decode())
+            break
+        else:
+            print(s.recv(1024).decode())
+        attempts = attempts-1    
+if attempts == 0:
+    print(f"Te has quedado sin intentos.")
+    print(f"El número secreto era: {code}")
 s.close()
--- ServidorTCP.py
+++ ServidorTCP.py
@@ -5,26 +5,31 @@ import random
 HOST = "localhost"
 PORT = 2000
 n = random.randint(1,10)
-print(f"Número secreto : {n}")
+print(f"Número secreto: {n}")
 #para comprobar si el puerto está escuchando netstat -a en cmd (conexiones activas ahora en el ordenador)
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.bind((HOST,PORT))#los dos parentesis son obligatorios
-s.listen()#abrimos servidor
+s.listen()
 print("Servidor abierto") 
 conn,addr = s.accept()#espera al cliente
+print(f"conexion con el cliente IP: {addr[0]} Puerto: {addr[1]} ha sido completada")
 code = "0"
+mess = ""
 while True:
-    data=conn.recv(1024)  
-    if int(data.decode())==n:
-        code = "1"
-        break
-    else:
-        if int(data.decode())<n:
-            conn.send("El número es mayor".encode())
-        else:
-            conn.send("El número es menor".encode())
+    data=conn.recv(1024)
+    if data.decode() != '':
+        if int(data.decode())==n:
+            code = "-1"
+            mess = "Has acertado, el número secreto era: "+str(n)
+        else:           
+            if int(data.decode())<n:
+                mess = "El número es mayor"
+            else:
+                mess = "El número es menor"
+            code = str(n)
         conn.send(code.encode())
-mess = "Has acertado, el número secreto era: "+str(n)
-conn.send(mess.encode())
-conn.send(code.encode())
+        conn.send(mess.encode())
+    else:
+        print(f"conexion con el cliente IP: {addr[0]} Puerto: {addr[1]} ha sido cerrada")
+        break
 conn.close()
